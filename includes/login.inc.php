da5087b1541bb1c7f5deca831c59de88ddb3d5bf
Paul Standley <paulstandley1972@gmail.com>
2019-06-07 23:21:21 +0100

    add error message

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -3,17 +3,18 @@
     require 'dbh.inc.php';
     $mailuid = $_POST['mailuid'];
     $password = $_POST['pwd'];
-    if(empty($mailuid) || empty($pwd)) {
+    $uid = $_POST['uid'];
+    if(empty($mailuid) || empty($password) || (empty($uid))) {
       header("Location: ../index.php?error=emptyfields");
       exit();
     }else{
-      $sql = "SELECT * FROM users WHERE uidUsers=? OR mailUsers;";
+      $sql = "SELECT * FROM users WHERE uidUsers=? OR mailUsers=?;";
       $stmt = mysqli_stmt_init($conn);
       if(!mysqli_stmt_prepare($stmt, $sql)) {
         header("Location: ..//index.php?error=sqlerror");
         exit();
       }else{
-        mysqli_stmt_bind_param($stmt, "ss", $mailuid, $mailuid);
+        mysqli_stmt_bind_param($stmt, "ss", $uid, $mailuid);
         mysqli_stmt_execute($stmt);
         $result = mysqli_stmt_get_result($stmt);
         if(!$row = mysqli_fetch_assoc($result)) {
@@ -30,6 +31,7 @@
             $_SESSION['userId'] = $row['idUsers'];
             $_SESSION['userUid'] = $row['uidUsers'];
             header("Location: ../index.php?error=wrongpwd");
+            exit();
           }else{
             header("Location ../index.php?error=wrongpwd");
             exit();
--- signup.php
+++ signup.php
@@ -4,6 +4,30 @@
 
   <main class="signup-main">
     <h2>SIGN UP</h2>
+    <?php 
+      if(isset($_GET['error'])) {
+        if($_GET['error']  == 'emptyfields') {
+          echo '<p class="signuperror">Fill in all fields!</p>';
+        }else
+        if($_GET['error']  == 'invaliduidmail') {
+          echo '<p class="signuperror">Invalid user name and email!</p>';
+        }else
+        if($_GET['error']  == 'invaliduid') {
+          echo '<p class="signuperror">Invalid user name!</p>';
+        }else
+        if($_GET['error']  == 'invalidmail') {
+          echo '<p class="signuperror">Inavlid email!</p>';
+        }else
+        if($_GET['error']  == 'passswordcheck') {
+          echo '<p class="signuperror">Passwords do not match!</p>';
+        }else
+        if($_GET['error']  == 'usertaken') {
+          echo '<p class="signuperror">User name taken!</p>';
+        }
+      }else if($_GET['signup'] == 'success') {
+        echo '<p class="signupsuccess">You have signed up</p>';
+      }
+    ?>
     <form action="includes/signup.inc.php" method="post">
       <input type="text" name="uid" placeholder="Username">
       <input type="email" name="mail" placeholder="E-mail">
